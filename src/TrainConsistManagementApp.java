1c17ed69bd4447c6c7fe4a2dedc2bb14d0964d49
athiraajay <athiraajay2005@gmail.com>
2026-04-01 09:43:15 +0530

    UC4:Maintain Ordered Bogie IDs (TreeSet & SortedSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,25 +1,27 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("==========================================");
-        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println(" UC4 - Maintain Ordered Bogie Consist ");
         System.out.println("==========================================\n");
-        Set<String> bogies = new HashSet<>();
-        bogies.add("B6101");
-        bogies.add("B6102");
-        bogies.add("B6103");
-        bogies.add("B6104");
-        bogies.add("B6101");
-        bogies.add("B6102");
-
-        System.out.println("Bogie IDs After Insertion:");
-        System.out.println(bogies);
+        LinkedList<String> trainConsist = new LinkedList<>();
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+        System.out.println("Initial Train Consist:");
+        System.out.println(trainConsist);
         System.out.println();
-
-        System.out.println("Note:");
-        System.out.println("Duplicates are automatically ignored by HashSet.");
-
-        System.out.println("\nUC3 uniqueness validation completed...");
+        trainConsist.add(2, "Pantry Car");
+        System.out.println("After Inserting 'Pantry Car' at position 2:");
+        System.out.println(trainConsist);
+        System.out.println();
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+        System.out.println("After Removing First and Last Bogie:");
+        System.out.println(trainConsist);
+        System.out.println("\nUC4 ordered consist operations completed...");
     }
 }
